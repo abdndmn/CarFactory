35ff0fe87d8ce327d662108e1b09feab6e487a42
abdndmn <abdndmn@hotmail.com>
2025-01-22 19:23:58 +0300

    Add project files.

---
+++ CarFactory/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFactory
+{
+    public class Car
+    {
+        
+
+            public DateTime ProductionDate { get; set; }
+            public string SerialNumber { get; set; }
+            public string Brand { get; set; }
+            public string Model { get; set; }
+            public string Color { get; set; }
+            public int DoorCount { get; set; }
+
+        
+
+        
+    }
+
+}
---
+++ CarFactory/Program.cs
@@ -0,0 +1,80 @@
+﻿using CarFactory;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        List<Car> cars = new List<Car>();
+
+        while (true)
+        {
+            Console.WriteLine("Araba üretmek istiyor musunuz? (E/H):");
+            string answer = Console.ReadLine().ToUpper();
+            
+            if (answer == "E")
+            {
+                Car car = new Car();
+
+                car.ProductionDate = DateTime.Now;
+                Console.WriteLine("Arabanın seri numarsını giriniz!");
+                car.SerialNumber = Console.ReadLine();
+                Console.WriteLine("Arabanın markasını giriniz!");
+                car.Brand = Console.ReadLine();
+                Console.WriteLine("Arabanın modelini giriniz!");
+                car.Model = Console.ReadLine();
+                Console.WriteLine("Arabanın rengini giriniz!");
+                car.Color = Console.ReadLine();
+
+                int userInput;
+                Console.WriteLine("Arabanın kapı sayısını giriniz!");
+                carDoorLoop:
+                    
+                    try
+                    {
+
+                        car.DoorCount = Convert.ToInt32(Console.ReadLine());
+
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Geçersiz giriş! Lütfen sadece bir tam sayı giriniz.");
+                        goto carDoorLoop;
+                    }
+                cars.Add(car);
+                Console.WriteLine("Araba üretildi!");
+
+
+
+            }
+            else if (answer == "H")
+            {
+                Console.WriteLine("Program sonlandırıldı.");
+                break;
+
+            }
+            else
+            {
+                Console.WriteLine("Hatalı giriş!");
+
+            }
+
+        }
+       
+        if (cars.Count != 0)
+        {
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"Seri Numarası: {car.SerialNumber}, Marka: {car.Brand}");
+            }
+
+        }
+
+
+
+
+
+    }
+}
+
+        
